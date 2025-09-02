cf778c9495f936db63b62dcdf4f9c1b94a9fb053
lelemustdie <pontiroli.lourdes@gmail.com>
2025-09-02 20:43:38 -0300

    AAAAAA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,8 +35,8 @@ export const metadata: Metadata = {
   },
   metadataBase: new URL("https://lourdespontiroli.com"),
   openGraph: {
-    title: "Cele y Mateo — ¡Nos casamos!",
-    description: "Fecha, lugar, RSVP y más.",
+    title: "CELE Y MATEO",
+    description: "¡NOS CASAMOS!",
     url: "/invitaciones-digitales",
     siteName: "lourdespontiroli.com",
     images: [
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
         url: "/og/cover.jpg", // coloca aquí un JPG/PNG 1200x630 público
         width: 1200,
         height: 630,
-        alt: "Anillo sobre la arena frente al mar",
+        alt: "Anillo",
       },
     ],
     type: "website",
@@ -52,8 +52,8 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Cele y Mateo — ¡Nos casamos!",
-    description: "Fecha, lugar, RSVP y más.",
+    title: "CELE Y MATEO",
+    description: "¡NOS CASAMOS!",
     images: ["/og/cover.jpg"],
   },
 };
