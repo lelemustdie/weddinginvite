89b641703b7969c60308490ec99f2ef0130d00d2
lelemustdie <pontiroli.lourdes@gmail.com>
2025-09-03 00:34:07 -0300

    metadata fixed

--- src/app/invitaciones-digitales/layout.tsx
+++ src/app/invitaciones-digitales/layout.tsx
@@ -17,11 +17,11 @@ export default function InvitacionesLayout({ children }: { children: ReactNode }
                 <meta name="description" content="¡NOS CASAMOS!" />
                 <meta property="og:title" content="CELE Y MATEO" />
                 <meta property="og:description" content="¡NOS CASAMOS!" />
-                <meta property="og:image" content="https://lourdespontiroli.com/og/cover-v3.jpg" />
-                <meta property="og:image:secure_url" content="https://lourdespontiroli.com/og/cover-v3.jpg" />
-                <meta property="og:image:type" content="image/jpeg" />
+                <meta property="og:image" content="https://lourdespontiroli.com/portada.webp" />
+                <meta property="og:image:secure_url" content="https://lourdespontiroli.com/portada.webp" />
+                <meta property="og:image:type" content="image/webp" />
                 <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
+                <meta property="og:image:height" content="800" />
                 <meta property="og:image:alt" content="Cele y Mateo - Invitación de Boda" />
                 <meta property="og:url" content="https://lourdespontiroli.com/invitaciones-digitales" />
                 <meta property="og:type" content="website" />
@@ -30,7 +30,7 @@ export default function InvitacionesLayout({ children }: { children: ReactNode }
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content="CELE Y MATEO" />
                 <meta name="twitter:description" content="¡NOS CASAMOS!" />
-                <meta name="twitter:image" content="https://lourdespontiroli.com/og/cover-v3.jpg" />
+                <meta name="twitter:image" content="https://lourdespontiroli.com/portada.webp" />
             </Head>
 
             <SimpleYouTubeAudio
